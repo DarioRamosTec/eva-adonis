641777579a69a3c0f0c168cb86ab801bc5910010
DarioRamosTec <dario.ramos.tec@gmail.com>
2023-10-18 01:28:32 -0600

    Mas cambios

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,19 +3,73 @@
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index ({ response }) {
-    const user = await User.all()
-    if (user.length > 0) {
-      response.accepted(
-        {
-          msg: 'Estas son todos los usuarios.',
-          data: user,
-        })
+  public async index ({ request, response }) {
+    let user
+    if (request.param('id', null) !== null) {
+      user = await User.all()
+      if (user.length > 0) {
+        response.accepted(
+          {
+            msg: 'Estas son todos los usuarios.',
+            data: user,
+          })
+      } else {
+        response.notFound(
+          {
+            msg: 'Aún no hay ningún usuario.',
+          })
+      }
     } else {
-      response.notFound(
-        {
-          msg: 'Aún no hay ningún usuario.',
-        })
+      user = await User.all()
+      if (user.length > 0) {
+        response.accepted(
+          {
+            msg: 'Estos son todos los usarios.',
+            data: user,
+          }
+        )
+      } else {
+        response.notFound(
+          {
+            msg: 'un no hay ningún usuario.',
+          }
+        )
+      }
+    }
+  }
+
+  public async store ({ request, response }) {
+    let user
+    if (request.param('id', null) !== null) {
+      user = await User.all()
+      if (user.length > 0) {
+        response.accepted(
+          {
+            msg: 'Estas son todos los usuarios.',
+            data: user,
+          })
+      } else {
+        response.notFound(
+          {
+            msg: 'Aún no hay ningún usuario.',
+          })
+      }
+    } else {
+      user = await User.all()
+      if (user.length > 0) {
+        response.accepted(
+          {
+            msg: 'Estos son todos los usarios.',
+            data: user,
+          }
+        )
+      } else {
+        response.notFound(
+          {
+            msg: 'un no hay ningún usuario.',
+          }
+        )
+      }
     }
   }
 }
